c145f8b59ae33762ee1395588c11c8ecd9d939cd
tyt51685168 <tyt51685168@gmail.com>
2022-03-23 14:50:35 +0800

    Update pivot_longer.r

--- tidyr/pivot_longer.r
+++ tidyr/pivot_longer.r
@@ -4,8 +4,28 @@ library(readr)
 
 head(relig_income)
 
+# A tibble: 6 x 11
+  religion           `<$10k` `$10-20k` `$20-30k` `$30-40k` `$40-50k` `$50-75k` `$75-100k` `$100-150k` `>150k` `Don't know/refused`
+  <chr>                <dbl>     <dbl>     <dbl>     <dbl>     <dbl>     <dbl>      <dbl>       <dbl>   <dbl>                <dbl>
+1 Agnostic                27        34        60        81        76       137        122         109      84                   96
+2 Atheist                 12        27        37        52        35        70         73          59      74                   76
+3 Buddhist                27        21        30        34        33        58         62          39      53                   54
+
+# 以下是最一般寬表格變長表格的做法
+# cols 指定所有要變成 variable name 的欄位
+# names_to 決定 variable name 的欄位名稱，cols 所指定的欄位其欄位名稱會變成此欄的內容(值)
+# values_to 決定 value 的欄位名稱，cols 所指定的欄位值會變成此欄的內容(值)
+
 relig_income %>%
   pivot_longer(cols = !religion, names_to = "income", values_to = "count")
+
+# A tibble: 180 x 3
+   religion income             count
+   <chr>    <chr>              <dbl>
+ 1 Agnostic <$10k                 27
+ 2 Agnostic $10-20k               34
+ 3 Agnostic $20-30k               60
+
 
 
 billboard 
