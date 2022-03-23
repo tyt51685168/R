255b5b5ca591d4b02f2c011ffaeb59184efcf798
tyt51685168 <tyt51685168@gmail.com>
2022-03-23 15:38:17 +0800

    Update pivot_longer.r

--- tidyr/pivot_longer.r
+++ tidyr/pivot_longer.r
@@ -11,10 +11,10 @@ head(relig_income)
 2 Atheist                 12        27        37        52        35        70         73          59      74                   76
 3 Buddhist                27        21        30        34        33        58         62          39      53                   54
 
-# 以下是最一般寬表格變長表格的做法
-# cols 指定所有要變成 variable name 的欄位
-# names_to 決定 variable name 的欄位名稱，cols 所指定的欄位其欄位名稱會變成此欄的內容(值)
-# values_to 決定 value 的欄位名稱，cols 所指定的欄位值會變成此欄的內容(值)
+### 以下是最一般寬表格變長表格的做法
+### cols 指定所有要變成 variable name 的欄位
+### names_to 決定 variable name 的欄位名稱，cols 所指定的欄位其欄位名稱會變成此欄的內容(值)
+### values_to 決定 value 的欄位名稱，cols 所指定的欄位值會變成此欄的內容(值)
 
 relig_income %>%
   pivot_longer(cols = !religion, names_to = "income", values_to = "count")
@@ -28,10 +28,9 @@ relig_income %>%
 
 
 
-# pivot_longer 針對複雜的欄位名稱也有很多處理方式，以下列舉介紹
+### pivot_longer 針對複雜的欄位名稱也有很多處理方式，以下列舉介紹
 
-
-# 1. 當你想要指定轉置的欄位有特殊 pattern，下例是開頭為 "wk" 的情況當作 variable
+### 1. 當你想要指定轉置的欄位有特殊 pattern，下例是開頭為 "wk" 的情況當作 variable
 billboard %>% 
   pivot_longer(
     cols = starts_with("wk"), 
@@ -61,11 +60,11 @@ billboard %>%
     values_drop_na = TRUE,
   )
 
-# 2. 當 variable name 有特殊 pattern 要處理，以此例來說，欄位名稱依照底線 _ 區分成三部分
-# names_to 就先指定 variable name 會有三個，接著透過 names_pattern 使用正規表達式取值
-# 得出來的結果就會是第一個底線前的欄位名稱被塞到 "diagnosis" 欄位中當值；中間的部分塞到 "gender" 欄位當值；底線的最後則是塞到 "age" 當值
+### 2. 當 variable name 有特殊 pattern 要處理，以此例來說，欄位名稱依照底線 _ 區分成三部分
+### names_to 就先指定 variable name 會有三個，接著透過 names_pattern 使用正規表達式取值
+### 得出來的結果就會是第一個底線前的欄位名稱被塞到 "diagnosis" 欄位中當值；中間的部分塞到 "gender" 欄位當值；底線的最後則是塞到 "age" 當值
 
-> who
+who
 # A tibble: 7,240 x 60
    country     iso2  iso3   year new_sp_m014 new_sp_m1524 new_sp_m2534 new_sp_m3544 new_sp_m4554 new_sp_m5564 new_sp_m65 new_sp_f014
    <chr>       <chr> <chr> <int>       <int>        <int>        <int>        <int>        <int>        <int>      <int>       <int>
@@ -91,12 +90,12 @@ who %>% pivot_longer(
 
 # -------------------------------------------------
 
-# 3. 以下為超特殊情形，欄位沒辦法單純的用分隔符號區分
-# 如果開頭是 dob (date of birth) 那該欄位的值就會是日期；如果開頭是 gender 那該欄位的值就會是1或2
-# 要是直接使用 pivot_longer 會出錯，因為兩種欄位的值資料型態不同
-# 用 names_transform 硬轉資料型態為 char 會過沒錯，但意義上這兩種值要被拆成兩個 variable 看待
-# 這裡介紹一個 pivot_longer (應該也是 tidyr) 的保留字  .value
-# 一樣是在 names_to 的時候先告訴程式即將要依照某種特殊 pattern 去拆欄位，再加上保留字 .value 讓程式去識讀不同的字眼要被拆成不同欄位(variable name)
+### 3. 以下為超特殊情形，欄位沒辦法單純的用分隔符號區分
+### 如果開頭是 dob (date of birth) 那該欄位的值就會是日期；如果開頭是 gender 那該欄位的值就會是1或2
+### 要是直接使用 pivot_longer 會出錯，因為兩種欄位的值資料型態不同
+### 用 names_transform 硬轉資料型態為 char 會過沒錯，但意義上這兩種值要被拆成兩個 variable 看待
+### 這裡介紹一個 pivot_longer (應該也是 tidyr) 的保留字  .value
+### 一樣是在 names_to 的時候先告訴程式即將要依照某種特殊 pattern 去拆欄位，再加上保留字 .value 讓程式去識讀不同的字眼要被拆成不同欄位(variable name)
 
 family <- tribble(
   ~family,  ~dob_child1,  ~dob_child2, ~gender_child1, ~gender_child2,
@@ -116,7 +115,7 @@ family %>%
     values_drop_na = TRUE
   )
 
-#得到的結果就會自動辨別 .value = dob, gender 並拆分成兩個 varible
+### 得到的結果就會自動辨別 .value = dob, gender 並拆分成兩個 varible
 # A tibble: 9 x 4
   family child  dob        gender
    <int> <chr>  <date>      <int>
