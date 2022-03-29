9d5114fe6c59d3bead67909ffc9d91def84d09d3
tyt51685168 <tyt51685168@gmail.com>
2022-03-29 16:27:35 +0800

    Update lapply interact with bracket.r

--- apply/lapply interact with bracket.r
+++ apply/lapply interact with bracket.r
@@ -3,4 +3,9 @@
 
 weather[[1]][1,1] # 一般 list 中 [ ] 括號取值的方法
 
-lapply(weather, "[", [1,1]) # 用 "[" 當作 function 參數，即可對 weather list 中每個 element 取出 [1,1] 的值
+lapply(weather, "[", 1, 1) # 用 "[" 當作 function 參數，即可對 weather list 中每個 element 取出 [1,1] 的值
+
+# 如果要取特定 row or column 也作得到
+lapply(weather, "[", 3, ) # 取第 3 row 
+lapply(weather, "[", , 3) # 取第 3 column
+
